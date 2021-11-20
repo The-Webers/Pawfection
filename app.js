df7dda979da589dc4603ad606727b04593074c32
Aarohi2110 <82254142+Aarohi2110@users.noreply.github.com>
2021-11-20 06:52:36 -0800

    final-commit

--- app.js
+++ app.js
@@ -1,25 +1,32 @@
 function dropdown() {
     document.getElementById("myDropdown").classList.toggle("show");
   }
+
+  
   
   function addProduct() {
     var logUser = localStorage.getItem("currentUser");
-    var proName = document.getElementById('proName').value
-    var proDes = document.getElementById('proDes').value
-    var proPrice = document.getElementById('proPrice').value
-    var proCateg = document.getElementById('proCateg')
-    var proCity = document.getElementById('proCity')
-    var img = document.getElementById('proImg').files[0];
-    var productError = document.getElementById('productError')
-    productError.innerHTML = ""
-    if (!proName || !proDes || !proPrice || proCateg.selectedIndex === 0 || proCity.selectedIndex === 0 || !img) {
-        return productError.innerHTML = "Please Fill Form Properly"
+    var parFName=document.getElementById('fname').value
+    var parLName=document.getElementById('lname').value
+    var email = document.getElementById('email').value
+    var contact = document.getElementById('contact').value
+    var petCateg = document.getElementById('petCateg')
+    var petName = document.getElementById('petName').value
+    var petAge = document.getElementById('petAge').value
+    var petBreed = document.getElementById('petBreed').value
+    var petLoc = document.getElementById('petLoc').value
+    var petDes = document.getElementById('petDes').value
+    var img = document.getElementById('petImg').files[0];
+    var petError = document.getElementById('petError')
+    petError.innerHTML = ""
+    if (!parFName ||!parLName || !email || !contact || !petDes || !petAge ||!petName ||!petBreed ||petCateg.selectedIndex === 0 ||!petLoc || !img) {
+        return petError.innerHTML = "Please Fill Form Properly"
     }
 
-    uploadImage(logUser, proName, proDes, proPrice, proCateg, proCity, img)
+    uploadImage(logUser,parFName, parLName, email, contact, petDes, petAge, petName, petBreed, petCateg, petLoc, img)
 }
 
-function uploadImage(logUser, proName, proDes, proPrice, proCateg, proCity, img) {
+function uploadImage(logUser,parFName, parLName, email, contact, petDes, petAge, petName, petBreed, petCateg, petLoc, img) {
     var imageName = img.name;
     var storageRef = firebase.storage().ref('images/' + imageName);
     var uploadTask = storageRef.put(img)
@@ -32,24 +39,28 @@ function uploadImage(logUser, proName, proDes, proPrice, proCateg, proCity, img)
     }, function () {
         uploadTask.snapshot.ref.getDownloadURL().then(function (downloadURL) {
             var imgURL = downloadURL
-            uploadProduct(imgURL, logUser, proName, proDes, proPrice, proCateg, proCity)
+            uploadProduct(imgURL, logUser,parFName, parLName, email, contact, petDes, petAge, petName, petBreed, petCateg, petLoc)
             resetValues()
         })
     })
 }
 
-function uploadProduct(imgURL, logUser, proName, proDes, proPrice, proCateg, proCity) {
+function uploadProduct(imgURL, logUser,parFName, parLName, email, contact, petDes, petAge, petName, petBreed, petCateg, petLoc) {
     // console.log("Hello")
     let database = firebase.database().ref('posts');
     let key = database.push().key;
     let post = {
         imgURL: imgURL,
         userId: logUser,
-        proName: proName,
-        proDes: proDes,
-        proPrice: proPrice,
-        proCateg: proCateg.value,
-        proCity: proCity.value,
+        parName:parFName+" "+parLName,
+        Email:email,
+        Contact:contact,
+        petName: petName,
+        petDes: petDes,
+        petAge: petAge,
+        petBreed:petBreed,
+        petCateg: petCateg.value,
+        petLoc: petLoc,
         postId: key,        
     }
     database.child(key).set(post)
@@ -57,13 +68,18 @@ function uploadProduct(imgURL, logUser, proName, proDes, proPrice, proCateg, pro
     // location.replace("../loginUser/my ads.html")
 }
 
-function resetValues(){
-    document.getElementById('proName').value = ""
-    document.getElementById('proDes').value = ""
-    document.getElementById('proPrice').value = ""
-    document.getElementById('proCateg').selectedIndex = 0
-    document.getElementById('proCity').selectedIndex = 0
-    //document.getElementById('proImg') = ""   
+function resetValues(){  
+    document.getElementById('fname').value= ""
+    document.getElementById('lname').value= ""
+    document.getElementById('email').value= ""
+    document.getElementById('contact').value= ""
+    document.getElementById('petCateg').selectedIndex = 0
+    document.getElementById('petName').value= ""
+    document.getElementById('petAge').value= ""
+    document.getElementById('petBreed').value= ""
+    document.getElementById('petLoc').value= ""
+    document.getElementById('petDes').value= ""
+    //document.getElementById('petImg')=null 
 }
 
 
@@ -107,51 +123,84 @@ function prepareCats(d) {
     }
     console.log(postId)
     for (var i = 0; i < postId.length; i++) {
-        var price = userPosts[postId[i]].proPrice
-        var description = userPosts[postId[i]].proDes
-        var location = userPosts[postId[i]].proCity
+        var Parname=userPosts[postId[i]].parName
+        var Email=userPosts[postId[i]].Email
+        var Contact=userPosts[postId[i]].Contact
+        var Name=userPosts[postId[i]].petName
+        var Age= userPosts[postId[i]].petAge
+        var Breed=userPosts[postId[i]].petBreed
+        var description = userPosts[postId[i]].petDes
+        var location = userPosts[postId[i]].petLoc
         var image = userPosts[postId[i]].imgURL
         var pid = userPosts[postId[i]].postId
-        if(userPosts[postId[i]].proCateg == "Cats"){
-            renderCats(price, description, location, image, pid)
-        }
+        if(userPosts[postId[i]].petCateg == "Cats"){
+         renderCats(Name,Breed,Age, description,Parname, location,Email,Contact, image, pid)
+              }
     }
 }
 
-function renderCats(price, description, location, image, pid){
+function renderCats(Name,Breed,Age, description,Parname, location,Email,Contact, image, pid){
     var productContainer = document.getElementById('productContainer')
-    var product = `<div class="col-3 my-3">
-        <div class="card" style="width: 18rem;">
-            <img class="card-img-top product_image" src=${image}>
-            <div class="card-body">
-              <h5 class="card-title">Rs ${price}</h5>
-              <small class="card-text" id="pid" style="visibility:hidden">${pid}</small>
-              <p class="card-text">${description}</p>
-              <p class="card-text">${location}</p>
-            </div>
+    var product = `<div class="pricing-column col-lg-4 col-md-6">
+    <div class="card">
+        <div class="card-body">
+        <img class="card-img-top product_image" src=${image}>
+        </div>
+        <div class="card-body">
+          <b><h3 class="card-title" style="color:green; font-weight: bolder;"> ${Name}</h3></b>
           </div>
-        </div>`
+          <div class="card-footer">
+          <h5 style=" font-weight: bolder;"> ${Breed}</h5>
+          </div>
+          <div class="card-footer">
+          <h6 style=" font-weight: bold;">Age: ${Age}</h6>
+          </div>
+          <div class="card-footer">
+          <p style=" font-weight: bold; class="card-text">${description}</p>
+          </div>
+          <div class="card-footer">
+          <p class="card-text">📍${location}</p>
+          </div>
+          <div class="card-footer">
+          <div style="width: 100%;">
+    <div style="width: 50%; float: left;"> 
+    📧 <br><a href = "mailto:${Email} style="align:left;">Send Email</a>
+    </div>
+    <div style="margin-left: 50%;"> 
+    📞<a href="tel:${Contact}" onclick="ga('send', 'event', { eventCategory: 'Contact', eventAction: 'Call', eventLabel: 'Mobile Button'});"><p class="call-button">Click to Call</p></a>
+    </div>
+    Owner: <h6 style=" font-weight: bolder;">${Parname}</h6>
+    <small class="card-text" id="pid" style="visibility:hidden">${pid}</small>
+    </div>
+
+    </div>`
         productContainer.innerHTML += product
     } 
+ 
 // End of Show Cats =============================================>   
 
-// Start of Show Dogs ===============================================>
+// // Start of Show Dogs ===============================================>
 function showDogs(){
+    console.log("yes")
     let promise = new Promise(function (resolve, reject) {
         firebase.database().ref('posts').on('value', function (data) {
             if (data.val()) {
+                //console.log("accept")
                 resolve(data.val())
+                
             } else {
+                console.log("reject")
                 reject("Something went wrong")
             }
         })
     })
 
     promise.then(function (data) {
-        // console.log(data)
+        console.log("yes")
+        console.log(data)
         prepareDogs(data)
-            // console.log("Check =>", check)
-            // addUser(check)
+             console.log("Check =>", check)
+            addUser(check)
         })
 
         .catch(function (error) {
@@ -161,39 +210,67 @@ function showDogs(){
 }
 
 function prepareDogs(d) {
+    console.log("yes")
     var userPosts = d
     var postId = [];
     for (var key in userPosts) {
-        // console.log(key)
-        // postId.push(key)
+         console.log(key)
+        //postId.push(key)
         postId.splice(0,0,key)
     }
-    // console.log(postId)
+    console.log(postId)
     for (var i = 0; i < postId.length; i++) {
-        var price = userPosts[postId[i]].proPrice
-        var description = userPosts[postId[i]].proDes
-        var location = userPosts[postId[i]].proCity
+        var Parname=userPosts[postId[i]].parName
+        var Email=userPosts[postId[i]].Email
+        var Contact=userPosts[postId[i]].Contact
+        var Name=userPosts[postId[i]].petName
+        var Age= userPosts[postId[i]].petAge
+        var Breed=userPosts[postId[i]].petBreed
+        var description = userPosts[postId[i]].petDes
+        var location = userPosts[postId[i]].petLoc
         var image = userPosts[postId[i]].imgURL
         var pid = userPosts[postId[i]].postId
-        if(userPosts[postId[i]].proCateg == "Dogs"){
-            renderDogs(price, description, location, image, pid)
-        }
+        if(userPosts[postId[i]].petCateg == "Dogs"){
+         renderDogs(Name,Breed,Age, description,Parname, location,Email,Contact, image, pid)
+              }
     }
 }
 
-function renderDogs(price, description, location, image, pid){
+function renderDogs(Name,Breed,Age, description,Parname, location,Email,Contact, image, pid){
     var productContainer = document.getElementById('productContainer')
-    var product = `<div class="col-3 my-3">
-        <div class="card" style="width: 18rem;">
-            <img class="card-img-top product_image" src=${image}>
-            <div class="card-body">
-              <h5 class="card-title">Rs ${price}</h5>
-              <small class="card-text" id="pid" style="visibility:hidden">${pid}</small>
-              <p class="card-text">${description}</p>
-              <p class="card-text">${location}</p>
-            </div>
+    var product = `<div class="pricing-column col-lg-4 col-md-6">
+    <div class="card">
+        <div class="card-body">
+        <img class="card-img-top product_image" src=${image}>
+        </div>
+        <div class="card-body">
+          <b><h3 class="card-title" style="color:green; font-weight: bolder;"> ${Name}</h3></b>
           </div>
-        </div>`
+          <div class="card-footer">
+          <h5 style=" font-weight: bolder;"> ${Breed}</h5>
+          </div>
+          <div class="card-footer">
+          <h6 style=" font-weight: bold;">Age: ${Age}</h6>
+          </div>
+          <div class="card-footer">
+          <p style=" font-weight: bold; class="card-text">${description}</p>
+          </div>
+          <div class="card-footer">
+          <p class="card-text">📍${location}</p>
+          </div>
+          <div class="card-footer">
+          <div style="width: 100%;">
+    <div style="width: 50%; float: left;"> 
+    📧 <br><a href = "mailto:${Email} style="align:left;">Send Email</a>
+    </div>
+    <div style="margin-left: 50%;"> 
+    📞<a href="tel:${Contact}" onclick="ga('send', 'event', { eventCategory: 'Contact', eventAction: 'Call', eventLabel: 'Mobile Button'});"><p class="call-button">Click to Call</p></a>
+    </div>
+    Owner: <h6 style=" font-weight: bolder;">${Parname}</h6>
+    <small class="card-text" id="pid" style="visibility:hidden">${pid}</small>
+    </div>
+
+    </div>`
         productContainer.innerHTML += product
     } 
 // End of Show Dogs =============================================> 
